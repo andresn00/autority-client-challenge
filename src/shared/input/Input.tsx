2170ed1a50121fe8bea5e663bea805b61b0f5175
andresn00 <andreslozada00@hotmail.com>
2023-12-03 23:31:43 -0500

    Input and textarea components

---
+++ src/shared/input/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import type { ComponentPropsWithoutRef } from 'react';
+
+type Props = ComponentPropsWithoutRef<'input'>
+
+
+export const Input = ({ children, ...attributes }: Props) => {
+  return (
+    <input className='px-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
+      {...attributes}>
+        {children}
+    </input>
+  )
+}
