164474ea3b096bc4f7e4b6531a54e53fc4ab64df
andresn00 <andreslozada00@hotmail.com>
2023-12-04 10:29:01 -0500

    Key frames for enterOpacity, enterRight

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        enterOpacity: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 'var(--opacity, 0.1)' },
+        },
+        enterRight: {
+          '0%': { 'margin-right': 'var(--mr-start, -50px)' },
+          '100%': { 'margin-right': 'var(--mr-end, 0px)' },
+        },
+      }
+    },
   },
   plugins: [],
 }
